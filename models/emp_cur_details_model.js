655dcc6350998539fe29632d4c0f6bd9751341cd
Ranjeet Saw <sawranjeet550@gmail.com>
2023-04-11 23:48:39 +0530

    Added changes as requested in Current employement details model

--- models/emp_cur_details_model.js
+++ models/emp_cur_details_model.js
@@ -1,17 +1,17 @@
 const { connector } = require("./databaseUtil");
 
-const empCurrentDetails_Schema = {
+const employeeCurrentEmployementSchema = {
   date_of_joining: { type: Date, required: true },
   department_name: { type: String, required: true },
   designation: { type: String, required: true },
   job_status: { type: String, required: true },
   job_profile: { type: String, required: true },
-  current_ctc: { type: String, required: true },
+  current_ctc: { type: Number, required: true },
 };
 
-const empCurrentDetails = new connector.model(
+const employeeCurrentEmployement = new connector.model(
   "empCurrentDetails",
-  empCurrentDetails_Schema
+  employeeCurrentEmployementSchema
 );
 
-module.exports = empCurrentDetails;
+module.exports = {};
