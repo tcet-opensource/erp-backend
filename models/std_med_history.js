7ace34ab2b2044e511b08c9ec99b1f02bd87932e
Shivam Tiwari <shivamn2003@gmail.com>
2023-04-12 15:55:06 +0530

    Created the student medical schema

--- models/std_med_history.js
+++ models/std_med_history.js
@@ -1,1 +1,16 @@
-xyz
+import { Schema } from 'mongoose';
+import { connector } from './databaseUtil';
+
+
+const studentMedicalSchema= new Schema({
+    bloodGroup:{type:String, required:true},
+    pastMedicalHistory:{type:String, required:true},
+    immunisationHistory:{type:String, required:true},
+    chronicMedicalConditions:{type:String, required:true},
+    parentsEmailId:{type:String, required:true},
+    parentsContact:{type:String, required:true},
+    relativeContacts:{type:String, required:true},
+})
+
+const medicalHistory=new connector.model('stu-med',studentMedicalSchema);
+module.exports={};
