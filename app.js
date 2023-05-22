b40f2c7b7d24b153e771e174ddbe7663f63cd608
Hitansh Doshi <hkd159@gmail.com>
2023-05-23 01:01:42 +0530

    added winston logger with basic configuration

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
-import path, {dirname} from "path";
-import logger from "morgan";
+import path, { dirname } from "path";
+import morgan from "morgan";
+import winston from "winston";
+const {combine, timestamp, align, printf, colorize, json} = winston.format;
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { fileURLToPath } from 'url';
@@ -14,8 +16,52 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+export let logger = winston.createLogger({
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            filename: './logs/all-logs.log',
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880,
+            maxFiles: 5,
+            colorize: false,
+            format: combine(
+                timestamp({
+                    format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+                }),
+                json()
+            )
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            format: combine(
+                colorize({all:true}),
+                timestamp({
+                    format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+                }),
+                align(),
+                printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+            ),
+            handleExceptions: true,
+            json: false,
+            colorize: true,
+        })
+    ],
+    exitOnError: false
+});
+
+logger.stream = {
+    write: function (message) {
+        logger.info(message.trim());
+    }
+};
+
+app.use(morgan(
+    ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status ":referrer" ":user-agent"', 
+    { "stream": logger.stream }));
+
 app.use(cors());
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
