c422f1cfd691748a010a8c1828dd7ee33810b5dd
Hitansh Doshi <hkd159@gmail.com>
2023-05-23 02:41:30 +0530

    added log file rotation system

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ import express from "express";
 import path, { dirname } from "path";
 import morgan from "morgan";
 import winston from "winston";
-const {combine, timestamp, align, printf, colorize, json} = winston.format;
+const { combine, timestamp, align, printf, colorize, json } = winston.format;
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { fileURLToPath } from 'url';
-
+import 'winston-daily-rotate-file';
 
 import indexRouter from "#routes/index";
 import usersRouter from "#routes/users";
@@ -16,27 +16,32 @@ import { logLevel } from "#constant";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const logFileTransport = new winston.transports.DailyRotateFile({
+    level: logLevel[process.env.ENVIRONMENT] || 'info',
+    filename: `./logs/application-${process.env.ENVIRONMENT}-%DATE%.log`,
+    handleExceptions: true,
+    json: true,
+    colorize: false,
+    format: combine(
+        timestamp({
+            format: 'DD-MM-YYYY hh:mm:ss.SSS A',
+        }),
+        json()
+    ),
+    datePattern: 'DD-MM-YYYY',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '30d'
+});
+
 export let logger = winston.createLogger({
     transports: [
-        new winston.transports.File({
-            level: logLevel[process.env.ENVIRONMENT] || 'info',
-            filename: './logs/all-logs.log',
-            handleExceptions: true,
-            json: true,
-            maxsize: 5242880,
-            maxFiles: 5,
-            colorize: false,
-            format: combine(
-                timestamp({
-                    format: 'YYYY-MM-DD hh:mm:ss.SSS A',
-                }),
-                json()
-            )
-        }),
+        logFileTransport,
         new winston.transports.Console({
             level: logLevel[process.env.ENVIRONMENT] || 'info',
             format: combine(
-                colorize({all:true}),
+                colorize({ all: true }),
                 timestamp({
                     format: 'YYYY-MM-DD hh:mm:ss.SSS A',
                 }),
@@ -58,7 +63,7 @@ logger.stream = {
 };
 
 app.use(morgan(
-    ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status ":referrer" ":user-agent"', 
+    ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status ":referrer" ":user-agent"',
     { "stream": logger.stream }));
 
 app.use(cors());
