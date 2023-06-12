54897cfc318132e925afe42b4ab2d203abf716f6
Vinit <vinitchawda20@gmail.com>
2023-06-12 16:50:47 +0530

    Added Uid into Schema

--- models/Accreditation.js
+++ models/Accreditation.js
@@ -1,10 +1,12 @@
 const {connector} = require('./databaseUtil');
 
 const accreditationSchema = {
-  accreditationName: {type: String,   required: true},
-  agencyName: {type: String,  required: true},
-  dateofAccreditation: {type: Date,   required: true},
-  dateofExpiry: {type: Date, required: true},
+    uid :{type : String, unique: true , required: true},
+    accreditationName: {type: String,   required: true}, 
+    agencyName: {type: String,  required: true},
+    dateofAccreditation: {type: Date,   required: true},
+    dateofExpiry: {type: Date, required: true}
+  
 }
 
 const AccreditationSchema = new connector.model('Accreditation_schema', accreditationSchema);
