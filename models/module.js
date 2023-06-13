0969fb76367e6b5501dc387bc06d4605a351ac31
Dhruv <dhruvrg2003@gmail.com>
2023-06-13 18:19:29 +0530

    Created module model

---
+++ models/module.js
@@ -0,0 +1,13 @@
+import connector from '#models/databaseUtil';
+
+const moduleSchema = {
+  modNo: { type: Number, required: true },
+  moduleName: { type: String, required: true },
+  moduleOutcome: { type: String, required: true },
+  moduleContents: [{ type: String, required: true }],
+  hrsPerModule: { type: Number, required: true },
+  cognitiveLevels: [{ type: String, required: true }],
+};
+
+const modules = new connector.model('Module', moduleSchema);
+export default modules;
