9e1eee8ffaeb3f2ba94403120db8e1b6e9d26880
Dhruv <dhruvrg2003@gmail.com>
2023-06-14 12:34:37 +0530

    CRUD operation added for module

---
+++ models/module.js
@@ -0,0 +1,57 @@
+import connector from '#models/databaseUtil';
+import { logger } from '#util';
+
+connector.set('debug', true);
+const moduleSchema = {
+  modNo: { type: Number, required: true },
+  moduleName: { type: String, required: true },
+  moduleOutcome: { type: String, required: true },
+  moduleContents: [{ type: String, required: true }],
+  hrsPerModule: { type: Number, required: true },
+  cognitiveLevels: [{ type: String, required: true }],
+};
+
+const moduleModel = new connector.model('Module', moduleSchema);
+
+async function remove(filter) {
+  const res = await moduleModel.findOneAndDelete(filter);
+  return res;
+}
+
+async function create(
+  modNo,
+  moduleName,
+  moduleOutcome,
+  moduleContents,
+  hrsPerModule,
+  cognitiveLevels
+) {
+  const module = new moduleModel({
+    modNo,
+    moduleName,
+    moduleOutcome,
+    moduleContents,
+    hrsPerModule,
+    cognitiveLevels,
+  });
+  const moduleDoc = await module.save();
+  return moduleDoc;
+}
+
+async function read(filter, limit = 1) {
+  const moduleData = await moduleModel.find(filter).limit(limit);
+  return moduleData;
+}
+
+async function update(filter, updateObject) {
+  const module = await moduleModel.findOneAndUpdate(filter, updateObject, {
+    new: true,
+  });
+  return module;
+}
+export default {
+  create,
+  read,
+  update,
+  remove,
+};
