2daf48e324bce58fb1440300ff723a4e5ecd9449
Vinit <vinitchawda20@gmail.com>
2023-06-14 10:44:52 +0530

    Performed Crud on AccreditationModel

--- models/accreditation.js
+++ models/accreditation.js
@@ -1,5 +1,7 @@
 import connector from "#models/databaseUtil";
+import { logger } from "#util";
 
+connector.set("debug", true);
 const accreditationSchema = {
     uid :{type : String, unique: true , required: true},
     accreditationName: {type: String,   required: true}, 
@@ -8,6 +10,34 @@ const accreditationSchema = {
     dateofExpiry: {type: Date, required: true}
 };
 
-const AccreditationModel = new connector.model('Accreditation', accreditationSchema);
+const Accreditation = new connector.model('Accreditation', accreditationSchema);
 
-export default AccreditationModel;
+async function remove(filter) {
+    const res = await Accreditation.findOneAndDelete(filter);
+    return res;
+}
+
+async function create(uid,accreditationName,agencyName,dateofAccreditation,dateofExpiry) {
+    const accreditation = new  Accreditation({
+        accreditationName,
+        agencyName,
+        uid,
+        dateofAccreditation,
+        dateofExpiry
+    });
+    const accreditationDoc = await accreditation.save();
+    return accreditationDoc;
+}
+
+async function read(filter, limit = 1) {
+    const accreditationData = await Accreditation.find(filter).limit(limit);
+    return accreditationData;
+}
+
+async function update(filter, updateObject) {
+    const accreditation = await Accreditation.findOneAndUpdate(filter, updateObject, { new : true});
+    return accreditation;
+}
+export default {
+    create, read, update, remove
+};
