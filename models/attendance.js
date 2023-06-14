90f5852b6a5e55d670d18110b845be747b36b638
Aastha Rai <aastharai2003@gmail.com>
2023-06-14 15:52:22 +0530

    #71 CRUD operation for attendance model

--- models/attendance.js
+++ models/attendance.js
@@ -1,15 +1,37 @@
 import connector from "./databaseUtil";
 import Infrastructure from "./infrastructure";
+
 connector.set("debug", true);
 
 const attendanceSchema = {
   date: { type: Date, required: true },
   time: { type: String, required: true },
+  absentees: { type: Array },
   class: Infrastructure,
 };
 
 const Attendance = connector.model("Attendance", attendanceSchema);
 
+async function create(date, time, absentees) {
+  const attendance = new Attendance({
+    date,
+    time,
+    absentees,
+  });
+  const newAttendence = await attendance.save();
+  return newAttendence;
+}
+
+async function remove(filter) {
+  const res = await Attendance.findOneAndDelete(filter);
+  return res;
+}
+
+async function grantAttendance(roll, date) {
+  const res = await Attendance.findOneAndUpdate(date, {$pull: { absentees: roll } }, { new: true });
+  return res;
+}
+
 export default {
-  Attendance,
+  create, remove, grantAttendance,
 };
