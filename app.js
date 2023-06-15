dd94b2ef3e60f49488b02e52d18f3d89d1885d75
Aastha Rai <aastharai2003@gmail.com>
2023-06-15 15:25:11 +0530

    #48 changes done as mentioned in PR

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from "express";
+import path, { dirname } from "path";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { fileURLToPath } from "url";
 import { logger } from "#util";
 import indexRouter from "#routes/index";
 import usersRouter from "#routes/users";
 import authRouter from "#routes/auth";
 
 const app = express();
+const currDirName = dirname(fileURLToPath(import.meta.url));
 
 app.use(morgan(
   ":remote-addr - :remote-user \":method :url HTTP/:http-version\" :status \":referrer\" \":user-agent\"",
@@ -18,9 +21,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(currDirName, "public")));
 
 app.use("/", indexRouter);
-
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 
