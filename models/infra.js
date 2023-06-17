ca0e3bc4dabd1b8141f162c51b116ce9a66e24e4
Ankit <115006624+ANKIT638-ux@users.noreply.github.com>
2023-06-17 18:48:49 +0530

    CRUD for infra

--- models/infra.js
+++ models/infra.js
@@ -1,7 +1,7 @@
 import connector from "#models/databaseUtil";
 
 const infrastructureSchema = {
-  infraID: { type: Number, required: true },
+  infraID: { type: Number, unique: true, required: true },
   infraName: { type: String, required: true },
   infraType: { type: String, required: true },
   infraWing: { type: String, required: true },
@@ -9,4 +9,36 @@ const infrastructureSchema = {
   capacity: { type: Number, required: true },
 };
 
-const infrastructureModel = new connector.model("Infrastructure", infrastructureSchema);
+const InfrastructureModel = new connector.model("Infrastructure", infrastructureSchema);
+
+async function remove(filter) {
+  const res = await InfrastructureModel.findOneAndDelete(filter);
+  return res;
+}
+
+async function create(infraID, infraName, infraType, infraWing, floor, capacity) {
+  const infrastructure = new InfrastructureModel({
+    infraID,
+    infraName,
+    infraType,
+    infraWing,
+    floor,
+    capacity,
+  });
+  const infrastructureDoc = await infrastructure.save();
+  return infrastructureDoc;
+}
+
+async function read(filter, limit = 1) {
+  const infrastructureData = await InfrastructureModel.find(filter).limit(limit);
+  return infrastructureData;
+}
+
+async function update(filter, updateObject) {
+  const infrastructure = await InfrastructureModel.findOneAndUpdate(filter, updateObject, { new: true });
+  return infrastructure;
+}
+
+export default {
+  create, read, update, remove,
+};
