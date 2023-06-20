88da4526785e6780330500c408439efb7616c78f
Shivam Tiwari <shivamn2003@gmail.com>
2023-06-20 23:57:33 +0530

    made Otpid to uid in otpschema

--- models/otpStore.js
+++ models/otpStore.js
@@ -2,7 +2,7 @@ import connector from "#models/databaseUtil";
 const {Schema}=connector;
 
 const otpStoreSchema=new Schema({
-    otpID:{type:String,unique:true,required:true},
+  uid:{type:String,unique:true,required:true},
     otp:{type:String,required:true,unique:true}  
 })
 
