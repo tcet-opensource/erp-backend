492d2e7a909b7fc5b0726d23ab042fd55646ea8c
Dhruv <dhruvrg2003@gmail.com>
2023-06-29 14:00:42 +0530

    module schema and CURD operation parameters updated

--- models/module.js
+++ models/module.js
@@ -1,15 +1,15 @@
-import connector from "#models/databaseUtil";
+import connector from '#models/databaseUtil';
 
 const moduleSchema = {
-  moduleNo: { type: Number, required: true },
-  moduleName: { type: String, required: true },
-  moduleOutcome: { type: String, required: true },
-  moduleContents: [{ type: String, required: true }],
+  No: { type: Number, required: true },
+  Name: { type: String, required: true },
+  Outcome: { type: String, required: true },
+  Contents: [{ type: String, required: true }],
   hrsPerModule: { type: Number, required: true },
   cognitiveLevels: [{ type: String, required: true }],
 };
 
-const Module = connector.model("Module", moduleSchema);
+const Module = connector.model('Module', moduleSchema);
 
 async function remove(filter) {
   const res = await Module.findOneAndDelete(filter);
@@ -17,18 +17,18 @@ async function remove(filter) {
 }
 
 async function create(
-  moduleNo,
-  moduleName,
-  moduleOutcome,
-  moduleContents,
+  No,
+  Name,
+  Outcome,
+  Contents,
   hrsPerModule,
-  cognitiveLevels,
+  cognitiveLevels
 ) {
   const module = new Module({
-    moduleNo,
-    moduleName,
-    moduleOutcome,
-    moduleContents,
+    No,
+    Name,
+    Outcome,
+    Contents,
     hrsPerModule,
     cognitiveLevels,
   });
