708dff5373c1c613016b2eb0e0f1b128a1d1c594
Astha Singh <singhastha182004@gmail.com>
2023-06-30 16:45:08 +0530

    #73 refactor and add crud to organisation model

--- models/organization.js
+++ models/organization.js
@@ -2,15 +2,40 @@ import connector from "./databaseUtil";
 
 const organizationSchema = {
   parent: { type: connector.Schema.Types.ObjectId, ref: "Organization", required: "true" },
-  orgID: { type: String, required: true },
-  orgName: { type: String, required: true },
-  orgAddress: { type: String, required: true },
-  orgInfra: [{ type: connector.Schema.Types.ObjectId, ref: "Infrastructure", required: "true" }],
+  startDate: { type: Date, required: true },
+  name: { type: String, required: true },
   accreditation: { type: connector.Schema.Types.ObjectId, ref: "Accrediation", required: "true" },
-  department: [{ type: connector.Schema.Types.ObjectId, ref: "Department", required: "true" }],
-  employees: [{ type: connector.Schema.Types.ObjectId, ref: "Faculty", required: "true" }],
-
 };
 
 // eslint-disable-next-line  no-unused-vars
 const Organization = connector.model("Organization", organizationSchema);
+
+async function remove(filter) {
+  const res = await Organization.findOneAndDelete(filter);
+  return res;
+}
+
+async function create(parent, startDate, name, accreditation) {
+  const org = new Organization({
+    parent,
+    startDate,
+    name,
+    accreditation,
+  });
+  const orgDoc = await org.save();
+  return orgDoc;
+}
+
+async function read(filter, limit = 1) {
+  const orgData = await Organization.find(filter).limit(limit);
+  return orgData;
+}
+
+async function update(filter, updateObject) {
+  const org = await Organization.findOneAndUpdate(filter, updateObject, { new: true });
+  return org;
+}
+
+export default {
+  create, read, update, remove,
+};
