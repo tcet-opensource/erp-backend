b9f2c11fb9de5f456b1027601e0cbe97fc9c0ebd
Dhruv <dhruvrg2003@gmail.com>
2023-06-30 16:01:47 +0530

    Created department model

---
+++ models/department.js
@@ -0,0 +1,19 @@
+import connector from '#models/databaseUtil';
+
+const departmentSchema = {
+  name: { type: Number, required: true },
+  acronym: { type: String, required: true, immutable: true },
+  yearOfStarting: { type: Date, immutable: true, required: true },
+  accreditations: {
+    type: connector.Schema.Types.ObjectId,
+    ref: 'Accreditation',
+    required: true,
+  },
+  infrastructures: {
+    type: connector.Schema.Types.ObjectId,
+    ref: 'Infrastructure',
+    required: true,
+  },
+};
+
+const Department = connector.model('Department', departmentSchema);
