f275449f116331a994a6c78a4c439f2392f64257
Abhishek Goyal <abhishekgoyal1311@gmail.com>
2023-07-01 10:55:20 +0530

    Create-practical-model

---
+++ models/practical.js
@@ -0,0 +1,15 @@
+import connector from "#models/databaseUtil";
+
+const practicalSchema = {
+  no: { type: Number, required: true },
+  type: { type: String, required: true },
+  title: { type: String, required: true },
+  hours: { type: Number, required: true },
+  cognitiveLevels: {
+    type: String,
+    required: true,
+    enum: ["L1", "L2", "L3", "L4", "L5", "L6"],
+  },
+};
+
+const Practical = connector.model("Practical", practicalSchema);
