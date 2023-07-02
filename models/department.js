c33f30460c2722b7dd10894a2848c7feabee764c
Vighnesh Mishra <vighnesh97531@gmail.com>
2023-07-03 00:06:55 +0530

    [ADDED] Added curd operation for Departemnt

--- models/department.js
+++ models/department.js
@@ -17,3 +17,46 @@ const departmentSchema = {
 };
 
 const Department = connector.model('Department', departmentSchema);
+
+//for creating
+async function create(
+  name,
+  acronym,
+  yearOfStarting,
+  accreditions,
+  infrastructures
+){
+    const department = new Department({
+     name,
+     acronym,
+     yearOfStarting,
+     accreditions,
+     infrastructures
+    });
+  const departmentDoc = await department.save();
+  return departmentDoc;
+  }
+
+  async function read(filter, limit = 1) {
+    const departmentData = await Department.find(filter).limit(limit);
+    return departmentData;
+  }
+
+  async function update(filter, updateObject) {
+    const department = await Department.findOneAndUpdate(filter, updateObject, {
+      new: true,
+    });
+    return department;
+  }
+
+  async function remove(filter) {
+    const res = await Department.findOneAndDelete(filter);
+    return res;
+  }
+  
+  export default {
+    create,
+    read,
+    update,
+    remove,
+  };  
