3084c3d50d43091dfb5cb6ad329f56349147b4a7
Hitansh Doshi <hkd159@gmail.com>
2023-07-02 21:31:54 +0530

    refactored the schema

--- models/tutorial.js
+++ models/tutorial.js
@@ -1,20 +1,15 @@
 import connector from "#models/databaseUtil";
-import { logger } from "#util";
 
-connector.set("debug", true);
 const tutorialSchema = {
-  no: { type: integer, required: true },
+  no: { type: Number, required: true },
   Title: { type: String, unique: true, required: true },
-  hours: { type: integer, required: true },
-cognitiveLevel:{
-  type:String,
-  enum:['L1','L2','L3','L4','L5','L6'],
-  default:'L1'
-}
+  hours: { type: Number, required: true },
+  cognitiveLevel:[{
+    type:String,
+    enum:['L1','L2','L3','L4','L5','L6'],
+    default:'L1'
+  }]
 };
 
-const tutorial= connector.model("tutorial", tutorialSchema);
-
-export default{tutorial}
-
+const Tutorial= connector.model("Tutorial", tutorialSchema);
 
