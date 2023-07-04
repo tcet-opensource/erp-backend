e48efc5e79946447f955a34116a9a3392ea2852a
Vikrant Singh <vikrantsingh202004@gmail.com>
2023-07-04 22:34:02 +0530

    137- feat: Create Topic model and schema with key-value pairs

--- models/topic.js
+++ models/topic.js
@@ -3,32 +3,5 @@ import connector from "#models/databaseUtil";
 const topicSchema = {
   title: { type: String, required: true },
 };
-
+// eslint-disable-next-line  no-unused-vars
 const Topic = connector.model("topic", topicSchema);
-
-async function create(title) {
-  const topic = new Topic({
-    title,
-  });
-  const topicDoc = await topic.save();
-  return topicDoc;
-}
-
-async function read(filter, limit = 1) {
-  const topicdata = await Topic.find(filter).limit(limit);
-  return topicdata;
-}
-
-async function update(filter, updateObject) {
-  const topic = await Topic.findOneAndUpdate(filter, updateObject, { new: true });
-  return topic;
-}
-
-async function remove(filter) {
-  const res = await Topic.findByIdAndDelete(filter);
-  return res;
-}
-
-export default {
-  create, read, remove, update,
-};
