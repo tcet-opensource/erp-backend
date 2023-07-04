967b67f135055e42f8a0e2a71df9c5ae14ec538e
Aastha Rai <aastharai2003@gmail.com>
2023-07-04 23:26:47 +0530

    Created get endpoint for accreditation

--- services/accreditation.js
+++ services/accreditation.js
@@ -1,13 +1,13 @@
-import accreditationModel from "#models/accreditation";
+import Accreditation from "#models/accreditation";
 import databaseError from "#error/database";
 
 export async function addNewAccreditation(name, agencyName, dateofAccreditation, dateofExpiry) {
-  const newAccreditation = await accreditationModel.create(
+  const newAccreditation = await Accreditation.create({
     name,
     agencyName,
     dateofAccreditation,
     dateofExpiry,
-  );
+  });
   if (newAccreditation.name === name) {
     return newAccreditation;
   }
@@ -15,7 +15,7 @@ export async function addNewAccreditation(name, agencyName, dateofAccreditation,
 }
 
 export async function getAccreditation(filter) {
-  const accreditation = await accreditationModel.read({ name: filter });
+  const accreditation = await Accreditation.read({ name: filter });
   if (accreditation) {
     return accreditation;
   }
