b1c43ff0b37d996392dbe42af02bba6fddaa7595
Abhishek Goyal <abhishekgoyal1311@gmail.com>
2023-07-05 22:07:04 +0530

    make_api_doc_in_apidoc.js_for_create_request_infrastructure/add

--- _apidoc.js
+++ _apidoc.js
@@ -1,19 +1,17 @@
-// ------------------------------------------------------------------------------------------
-// General apiDoc documentation blocks and old history blocks.
-// ------------------------------------------------------------------------------------------
-
-// ------------------------------------------------------------------------------------------
-// Current Success.
-// ------------------------------------------------------------------------------------------
-
-// ------------------------------------------------------------------------------------------
-// Current Errors.
-// ------------------------------------------------------------------------------------------
-
-// ------------------------------------------------------------------------------------------
-// Current Permissions.
-// ------------------------------------------------------------------------------------------
-
-// ------------------------------------------------------------------------------------------
-// History.
-// ------------------------------------------------------------------------------------------
+/**
+ * @api {post} /infrastructure/add Add Infrastructure
+ * @apiName AddInfrastructure
+ * @apiGroup Infrastructure
+ *
+ * @apiParam {String} name The name of the infrastructure.
+ * @apiParam {String} type The type of the infrastructure.
+ * @apiParam {String} wing The wing where the infrastructure is located.
+ * @apiParam {Number} floor The floor where the infrastructure is located.
+ * @apiParam {Number} capacity The capacity of the infrastructure.
+ *
+ * @apiSuccess {String} res Success message with the ID of the added infrastructure.
+ *
+ * @apiError (Error 500) DatabaseError Error while inserting in the database.
+ *
+ * @apiDescription Adds a new infrastructure to the system.
+ */
