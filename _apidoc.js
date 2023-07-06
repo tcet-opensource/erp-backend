3329a38c2a3f5219ec98ab7e7d950b63c5710844
Aryanzs <supersaiyanaryan@gmail.com>
2023-07-06 20:46:06 +0530

    [Added] added apidoc for accrediation/add

--- _apidoc.js
+++ _apidoc.js
@@ -17,3 +17,29 @@
 // ------------------------------------------------------------------------------------------
 // History.
 // ------------------------------------------------------------------------------------------
+/**
+ * @api {post} /accreditation/add Add Accreditation
+ * @apiName AddAccreditation
+ * @apiGroup Accreditation
+ * @apiDescription Add a new accreditation.
+ *
+ * @apiBody {String} name Accreditation name.
+ * @apiBody {String} agencyName Agency name.
+ * @apiBody {Date} dateofAccreditation Date of accreditation.
+ * @apiBody {Date} dateofExpiry Date of expiry.
+ *
+ * @apiSuccess {String} res Response message.
+ * @apiError (Error 500) UserNotFound The  of the User was not found
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "res": "added accreditation Example Accreditation"
+ *     }
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 500 Internal Server Error
+ *     {
+ *       "err": "Error while inserting in DB"
+ *     }
+ */
