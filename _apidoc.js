f634d96121efe8b847883ee1d5db094806cbba42
Vighnesh Mishra <vighnesh97531@gmail.com>
2023-07-06 20:15:06 +0530

    [Create] Create a apidoc for Get Infrastructure

--- _apidoc.js
+++ _apidoc.js
@@ -17,3 +17,21 @@
 // ------------------------------------------------------------------------------------------
 // History.
 // ------------------------------------------------------------------------------------------
+/**
+ * @api {get} infrastructure/list Request Infrastructure List
+ * @apiName GetInfrastructure
+ * @apiGroup Infrastructure
+ *
+ * @apiQuery {String} name Name of Infrastructure .
+ * @apiQuery {String} type Type of Infrastructure. One of possible Lab, Classroom.
+ * @apiQuery {String} wing Wing of Infrastructure.
+ * @apiQuery {Number} floor Floor of Infrastructure.
+ * @apiQuery {Number} capacity Capacity of Infrastructure.
+ * 
+ * @apiSuccess {Infrastructure[]} res Array of Filtered Infrastructure Doc .
+ * @apiSuccess {String} infrastructure.name Name of Infrastructure
+ * @apiSuccess {String} infrastructure.type Type of Infrastructure.
+ * @apiSuccess {String} infrastructure.wing Wing of Infrastructure.
+ * @apiSuccess {Number} infrastructure.floor Floor of Infrastructure.
+ * @apiSuccess {Number} infrastructure.capacity Capacity of Infrastructure.
+ */
